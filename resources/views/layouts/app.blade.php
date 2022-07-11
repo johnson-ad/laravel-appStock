3f20096beb20996dadb9826113fccc80cf49e613
='Johnson-ad <=>
2022-07-11 16:49:04 +0000

    Petit souci avec le webpack car ayant laravel version 9 et aussi initailisation a la base de donner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mon super site</title>
+    {{ asset('css/app.css') }}
 </head>
 <body>
     <!--include permet d'appeler des composant bien specifique -->
     @include('partials.navbar')
     <!-- le @ yield permet d'indiquer kon aura des contenuent dynamique -->
     @yield('content')
+    
+    {{ asset('js/app.js') }}
 </body>
 </html>
